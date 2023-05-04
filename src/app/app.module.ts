bc63d6a4a4d647c30abd7504030ad6183f78e1a1
Angel HS <ad.hernandezsa@gmail.com>
2023-05-04 17:22:16 -0600

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
+import { AppComponent } from './app.component';
+import { DbzService } from './dbz/services/dbz.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    DbzModule
+  ],
+  providers: [DbzService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
